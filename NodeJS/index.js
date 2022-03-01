efc1289d4f518ff5fc328d8da9cbf66b1b322e9f
Marco Serenelli <marco.serenelli00@gmail.com>
2022-03-01 23:32:44 +0100

    - Added new packages:   + bcrypt for password hashing   + dotenv for .env files   + jsonwebtoken for token service
    - Removed redundant .gitignore
    - Added .env files to .gitignore
    - Created loginController to handle login (works)
    - Changed user creation to hash passwords (register)

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -6,7 +6,7 @@ const { mongoose } = require('./db.js');
 var userController = require('./Controllers/userController');
 var productController = require('./Controllers/productController');
 var categoryController = require('./Controllers/categoryController');
-
+var loginController = require('./Controllers/loginController');
 var app = express();
 
 app.use(bodyParser.json());
@@ -16,5 +16,6 @@ app.listen(3000, ()=>console.log("Server started on port 3000"));
 app.use('/user',userController);
 app.use('/products',productController);
 app.use('/categories',categoryController);
+app.use('/login',loginController);
 
 

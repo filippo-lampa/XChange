5251e6dff9c734f0122959f1d47e70ffe0e0df16
Marco Serenelli <marco.serenelli00@gmail.com>
2022-03-11 14:43:27 +0100

    Users can delete/modify their products from add-product menu
    - Delete/Modify user products
    - Changed console.log to page refreshes

--- Angular/src/app/add-product/add-product.component.ts
+++ Angular/src/app/add-product/add-product.component.ts
@@ -20,9 +20,20 @@ export class AddProductComponent implements OnInit {
     imageUrl: null
   }
 
+  formToModify: any = {
+    name: null,
+    category: null,
+    description: null,
+    imageUrl: null
+  }
+
   categoriesList!: Category[];
-  isLoaded = false;
+  isLoaded: Boolean = false;
   selectedCategory!: string;
+  productList!: Product[];
+  selectedProduct!: string;
+  showDeleteProduct: Boolean = false;
+  productToModify!: Product;
 
   constructor(private tokenStorageService: TokenStorageService, private productService: ProductService, private categoryService: CategoryService) { }
 
@@ -30,8 +41,19 @@ export class AddProductComponent implements OnInit {
     this.categoryService.getCategoriesList().subscribe(
       data => {
         this.categoriesList = data as Category[];
-        console.log(this.categoriesList);
         this.isLoaded = true;
+      },
+      err => {
+        console.log(err.error.message);
+      }
+    )
+
+    this.productService.getUserProducts(this.tokenStorageService.getUserId()).subscribe(
+      data => {
+        this.productList = data as Product[];
+        if (this.productList.length > 0) {
+          this.showDeleteProduct = true;
+        }
       },
       err => {
         console.log(err.error.message);
@@ -40,29 +62,82 @@ export class AddProductComponent implements OnInit {
   }
 
   onAddProduct() {
-    const product: Product = {
-      name: this.form.name,
-      category: this.selectedCategory,
-      description: this.form.description,
-      views: 0,
-      uploadDate: new Date(),
-      imageUrl: this.form.imageUrl,
-      sellerId: this.tokenStorageService.getUserId()
-    }
-
-    this.productService.postProduct(product, this.tokenStorageService.getUserId()).subscribe(
-      data => {
-        console.log("ok");
-      },
-      err => {
-        console.log(err.error.message);
+    if (this.selectedCategory != undefined || this.selectedCategory != null) {
+      const product: Product = {
+        name: this.form.name,
+        category: this.selectedCategory,
+        description: this.form.description,
+        views: 0,
+        uploadDate: new Date(),
+        imageUrl: this.form.imageUrl,
+        sellerId: this.tokenStorageService.getUserId()
       }
-    )
-
+      this.productService.postProduct(product, this.tokenStorageService.getUserId()).subscribe(
+        data => {
+          this.selectedCategory = '';
+          this.ngOnInit();
+        },
+        err => {
+          console.log(err.error.message);
+        }
+      )
+    }
   }
 
   selectCategory(category: string) {
     this.selectedCategory = category;
+  }
+
+  onDeleteProduct() {
+    if (this.selectedProduct != undefined || this.selectedProduct != null) {
+      this.productService.deleteProduct(this.selectedProduct, this.tokenStorageService.getUserId()).subscribe(
+        data => {
+          this.selectedProduct = '';
+          this.ngOnInit();
+        },
+        err => {
+          console.log(err.error.message);
+        }
+      )
+    }
+  }
+
+  selectProduct(product: string) {
+    this.selectedProduct = product;
+  }
+
+  selectProductToModify(product: Product) {
+    this.productToModify = product;
+    this.formToModify.name = product.name;
+    this.formToModify.category = product.category;
+    this.formToModify.description = product.description;
+    this.formToModify.imageUrl = product.imageUrl;
+  }
+
+  onModifyProduct() {
+    if (this.productToModify != undefined || this.productToModify != null) {
+      const product: Product = {
+        _id: this.productToModify._id,
+        name: this.formToModify.name,
+        category: this.selectedCategory,
+        description: this.formToModify.description,
+        views: this.productToModify.views,
+        uploadDate: this.productToModify.uploadDate,
+        imageUrl: this.formToModify.imageUrl,
+        sellerId: this.productToModify.sellerId
+      }
+
+      this.productService.putProduct(product, this.tokenStorageService.getUserId()).subscribe(
+        data => {
+          this.selectedCategory = '';
+          this.ngOnInit();
+        },
+        err => {
+          console.log(err.error.message);
+        }
+      )
+    }
+
   }
 
 }
